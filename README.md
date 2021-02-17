995488cf37dcd532ab9e71e408d2291e69e23202
Marc Doerflinger <marc@doerf.li>
2021-02-17 23:37:12 +0100

    add scraper

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # foph-covid19-scraper
+
+Scraper for covid 19 vaccination data provided by FOPH via https://www.covid19.admin.ch/en/overview
---
+++ scraper.py
@@ -0,0 +1,66 @@
+from urllib.request import urlopen
+import re
+import zipfile
+from io import BytesIO
+import csv
+from io import TextIOWrapper
+
+prefix = "https://www.covid19.admin.ch"
+overview_src = urlopen("%s/en/overview" % prefix).read().decode('utf-8')
+prog = re.compile("href=\"(/api/data/(.+)/sources-csv.zip)\"")
+match = prog.search(overview_src)
+# print(match)
+# print(match.group(1))
+data_url = "%s%s" % (prefix, match.group(1))
+print("data_url: %s" % data_url)
+
+vacc_data = {}
+
+filebytes = BytesIO(urlopen(data_url).read())
+fophdatazip = zipfile.ZipFile(filebytes)
+for name in fophdatazip.namelist():
+    if (name in ["data/COVID19FullyVaccPersons.csv", "data/COVID19VaccDosesAdministered.csv", "data/COVID19VaccDosesDelivered.csv"]):
+        # print(name)
+        # for line in fophdatazip.open(name).readlines():
+        #     print(line)
+        csvreader = csv.reader(TextIOWrapper(fophdatazip.open(name), 'utf-8'), delimiter=',', quotechar='"')
+        for row in csvreader:
+            if row[0] == "geoRegion":
+                continue
+            # print(', '.join(row))
+            canton = row[0]
+            date = row[1]
+            total = row[3]
+            per100 = row[4]
+            dtype = row[5]
+            if date not in vacc_data:
+                vacc_data[date] = {}
+            if canton not in vacc_data[date]:
+                vacc_data[date][canton] = {}
+            if dtype == "COVID19VaccDosesDelivered":
+                vacc_data[date][canton]["deliveredTotal"] = total
+                vacc_data[date][canton]["deliveredPer100"] = per100
+            elif dtype == "COVID19VaccDosesAdministered":
+                vacc_data[date][canton]["administeredTotal"] = total
+                vacc_data[date][canton]["administeredPer100"] = per100
+            elif dtype == "COVID19FullyVaccPersons":
+                vacc_data[date][canton]["fullyVaccTotal"] = total
+                vacc_data[date][canton]["fullyVaccPer100"] = per100
+
+# print(vacc_data)
+
+with open('vacc_data.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    for date in sorted(vacc_data):
+        print("writing data for %s" % date)
+        for canton in sorted(vacc_data[date]):
+            data = vacc_data[date][canton]
+            # print(data)
+            dt = data["deliveredTotal"]
+            dp = data["deliveredPer100"]
+            at = data["administeredTotal"]
+            ap = data["administeredPer100"]
+            ft = data["fullyVaccTotal"] if ("fullyVaccTotal" in data) else ""
+            fp = data["fullyVaccPer100"] if ("fullyVaccPer100" in data) else ""
+            csvwriter.writerow([date, canton, dt, dp, at, ap, ft, fp])
